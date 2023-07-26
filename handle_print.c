82dc329e3e80574b68e67cf6fac01181bfb41a6a
Scholarbee <scholarbee6@gmail.com>
2023-07-25 19:20:38 -0700

    I'm not going anywhere.

--- handle_print.c
+++ handle_print.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
+ * handle_print - This Prints an arg based on its type
+ * @fmt: This is the formatted str in which to print the arg.
+ * @list: The list of args to be printed.
+ * @ind: the ind ....
+ * @buffer: The buffer aray to handle_print.
+ * @flags: this calculates the active_flags.
+ * @width: to_get_width
+ * @precision: the Precision_specification
+ * @size: the Size_specifier
+ * Return: this returns (1 or 2);
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
